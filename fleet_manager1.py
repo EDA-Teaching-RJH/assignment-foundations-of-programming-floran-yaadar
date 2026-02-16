cf2bcf234b09c11da2bfaaee1b8244633b0a6623
floran-yaadar <feky2@kent.ac.uk>
2026-02-16 02:21:46 +0000

    redid and done add member and remove member

--- fleet_manager1.py
+++ fleet_manager1.py
@@ -1,6 +1,13 @@
 def main():
-    name, rank, divs, ids = init_database()
-    display_menu()
+    names, ranks, divs, ids = init_database()
+    while True:
+        option = display_menu()
+        if option == 1:
+         add_member(names, ranks, divs, ids)
+        elif option ==2:
+            remove_member(names, ranks, divs, ids)
+        elif option ==3:
+            update_rank(names, ranks, ids)
 def init_database():
     names = ["James T.Kirk", "Leonard McCoy", "Spock", "Montgomery Scott", "Hikaru Sulu", "Nyota Uhura", "Pavel Chekov"]
     ranks = ["Captain", "Lt. Commander", "Commander", "Lt. Commander", "Lieutenant", "Lieutenant", "Ensign" ]
@@ -13,5 +20,34 @@ def display_menu():
     option = int(input("1 -Add Member\n2 -remove member\n3 -update rank\n4 -display roster\n5 -search crew\n6 -filter by division\n7 -calculate payroll\n8 -Count officers\n9 -Accessthe Database"))
     print(option)
     return option
+def add_member(names, ranks, divs, ids):
+    print("You have selected add member")
+    name_new = input("What is the name of the new memeber? ")
+    rank_new = input("What is the rank of the new member?  ")
+    div_new = input("What is the division of the new member? ")
+    id_new = input("what is the ID of the new member? ")
+    if id_new in (ids):
+        print("This ID is already in use. Please enter a uniue ID.")
+        add_member(names, ranks, divs, ids)
+    else:
+        names.append(name_new)
+        ranks.append(rank_new)
+        divs.append(div_new)
+        ids.append(id_new)
+        print(f"{name_new} has been added to the crew roster.")
+def remove_member(names, ranks, divs, ids):
+    print("you have selected remove member")
+    name_to_remove = input("what is the name of the member you want to remove? ")
+    name_to_remove = name_to_remove.title()
+    if name_to_remove in names:
+        index = names.index(name_to_remove)
+        names.pop(index)
+        ranks.pop(index)
+        divs.pop(index)
+        ids.pop(index)
+        print(f"{name_to_remove} has been removed from the crew roster.")
+    else:
+        print(f"{name_to_remove} cannot be found in the crew roster")
+def update_rank(names, ranks, ids):
 main()
 
